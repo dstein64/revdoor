0ed63cd479c8b187861b2b3a330629447fa572da
Dan <dstein64@users.noreply.github.com>
2020-02-10 16:24:57 -0500

    Update tests.

--- tests/test_revdoor.cpp
+++ tests/test_revdoor.cpp
@@ -1,20 +1,92 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
+
 #include <revdoor.hpp>
+#include <utility>
+#include <vector>
 
-BOOST_AUTO_TEST_CASE( test1 ) {
-    // reports 'error in "test1": test 2 == 1 failed'
-    BOOST_CHECK( 2 == 1 );
+using namespace std;
+
+BOOST_AUTO_TEST_CASE(test_combinations) {
+  int n = 5;
+  int t = 3;
+  revdoor::combinations combs(n, t);
+  vector<int> init_state = combs.state();
+  vector<int> expected_init_state = {0, 1, 2};
+  BOOST_CHECK_EQUAL_COLLECTIONS(
+      init_state.begin(),
+      init_state.end(),
+      expected_init_state.begin(),
+      expected_init_state.end());
+  vector<pair<int,int>> swaps;
+  vector<pair<int,int>> expected_swaps = {
+    {1, 3}, {0, 1}, {2, 0}, {1, 4}, {0, 1},
+    {1, 2}, {3, 0}, {0, 1}, {2, 0}
+  };
+  int out, in;
+  while (combs.step(&out, &in)) {
+    swaps.push_back({out, in});
+  }
+  BOOST_REQUIRE_EQUAL(swaps.size(), expected_swaps.size());
+  for (int i = 0; i < swaps.size(); ++i) {
+    BOOST_CHECK_EQUAL(swaps[i].first, expected_swaps[i].first);
+    BOOST_CHECK_EQUAL(swaps[i].second, expected_swaps[i].second);
+  }
+  vector<int> end_state = combs.state();
+  vector<int> expected_end_state = {0, 1, 4};
+  BOOST_CHECK_EQUAL_COLLECTIONS(
+      end_state.begin(),
+      end_state.end(),
+      expected_end_state.begin(),
+      expected_end_state.end());
 }
 
-BOOST_AUTO_TEST_CASE( test2 )
-{
-    int i = 0;
-
-    // reports 'error in "test2": check i == 2 failed [0 != 2]'
-    BOOST_CHECK_EQUAL( i, 2 );
-
-    BOOST_CHECK_EQUAL( i, 0 );
+BOOST_AUTO_TEST_CASE(test_combinations_with_replacement) {
+  int n = 5;
+  int t = 3;
+  revdoor::combinations_with_replacement combs(n, t);
+  vector<int> init_state = combs.state();
+  vector<int> expected_init_state = {0, 0, 0};
+  BOOST_CHECK_EQUAL_COLLECTIONS(
+      init_state.begin(),
+      init_state.end(),
+      expected_init_state.begin(),
+      expected_init_state.end());
+  vector<vector<int>> swaps;
+  vector<vector<int>> expected_swaps = {
+    {0, 1, 0, 1}, {0, 1}, {1, 0, 1, 0}, {0, 2, 1, 2}, {0, 1},
+    {1, 2}, {2, 1, 2, 0}, {0, 1}, {1, 0, 1, 0}, {0, 3, 2, 3},
+    {0, 1}, {1, 2}, {2, 3}, {3, 2, 3, 0}, {0, 1}, {1, 2},
+    {2, 1, 2, 0}, {0, 1}, {1, 0, 1, 0}, {0, 4, 3, 4}, {0, 1},
+    {1, 2}, {2, 3}, {3, 4}, {4, 3, 4, 0}, {0, 1}, {1, 2},
+    {2, 3}, {3, 2, 3, 0}, {0, 1}, {1, 2}, {2, 1, 2, 0}, {0, 1},
+    {1, 0, 1, 0}
+  };
+  int out1, in1, out2, in2;
+  while (combs.step(&out1, &in1, &out2, &in2)) {
+    vector<int> v;
+    if (out1 != in1) {
+      v.push_back(out1);
+      v.push_back(in1);
+    }
+    v.push_back(out2);
+    v.push_back(in2);
+    swaps.push_back(v);
+  }
+  BOOST_REQUIRE_EQUAL(swaps.size(), expected_swaps.size());
+  for (int i = 0; i < swaps.size(); ++i) {
+    BOOST_CHECK_EQUAL_COLLECTIONS(
+        swaps[i].begin(),
+        swaps[i].end(),
+        expected_swaps[i].begin(),
+        expected_swaps[i].end());
+  }
+  vector<int> end_state = combs.state();
+  vector<int> expected_end_state = {0, 0, 4};
+  BOOST_CHECK_EQUAL_COLLECTIONS(
+      end_state.begin(),
+      end_state.end(),
+      expected_end_state.begin(),
+      expected_end_state.end());
 }
-
